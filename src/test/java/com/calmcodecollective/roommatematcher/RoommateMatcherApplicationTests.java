fd0d6514cd5c372d17a3d604c5b4efbc5e5f820b
Yash Deshpande <yash@Yashs-MacBook-Air.local>
2024-01-25 22:43:07 -0800

    Add more dependencies and change package name

--- src/test/java/com/calmcodecollective/roommatematcher/RoommateMatcherApplicationTests.java
+++ src/test/java/com/calmcodecollective/roommatematcher/RoommateMatcherApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.calmcodecollective.roommatematcher;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RoommateMatcherApplicationTests {
 
 	@Test
 	void contextLoads() {
