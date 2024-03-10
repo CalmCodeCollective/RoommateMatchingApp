59ff53afb0e6a556f2bf81b390274f2d38e85974
Yash Deshpande <yash@Yashs-MacBook-Air.local>
2024-03-10 05:07:57 -0700

    return response as Entity

--- src/main/java/com/calmcodecollective/roommatematcher/controller/AccountController.java
+++ src/main/java/com/calmcodecollective/roommatematcher/controller/AccountController.java
@@ -1,9 +1,10 @@
 package com.calmcodecollective.roommatematcher.controller;
 
+import com.calmcodecollective.roommatematcher.exception.AccountNotFoundException;
 import com.calmcodecollective.roommatematcher.model.roommate.Account;
 import com.calmcodecollective.roommatematcher.model.roommate.AccountRepository;
-
-import com.calmcodecollective.roommatematcher.exception.AccountNotFoundException;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,20 +14,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class AccountController {
     private final AccountRepository repository;
+    private final AccountModelAssembler assembler;
 
-    public AccountController(AccountRepository repository) {
+    public AccountController(AccountRepository repository, AccountModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
     // tag::get-aggregate-root[]
     @GetMapping("/accounts")
-    List<Account> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Account>> all() {
+        List<EntityModel<Account>> accounts = repository.findAll().stream().map(assembler::toModel).collect(Collectors.toList());
+
+        return CollectionModel.of(accounts, linkTo(methodOn(AccountController.class).all()).withSelfRel());
     }
     // end::get-aggregate-root[]
 
@@ -38,10 +47,11 @@ public class AccountController {
     // Single item
 
     @GetMapping("/accounts/{id}")
-    Account one(@PathVariable Long id) {
+    EntityModel<Account> one(@PathVariable Long id) {
 
-        return repository.findById(id)
+        Account account = repository.findById(id)
                 .orElseThrow(() -> new AccountNotFoundException(id));
+        return assembler.toModel(account);
     }
 
     @PutMapping("/accounts/{id}")
